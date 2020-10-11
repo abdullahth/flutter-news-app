2e4b804eb7ad31692caff1bb60a6072a1febb9b3
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-10-11 23:31:05 +0200

    Commit 1

--- app/lib/screens/home/home.dart
+++ app/lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:app/services/fetch-data-service.dart';
 import 'package:flutter/material.dart';
 
 import '../../config-dim.dart';
@@ -14,46 +15,86 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     DimsConfig.init(context);
+
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: RichText(
-          text: TextSpan(
-            text: "News App",
-            style: TextStyle(
-                fontSize: 18, fontFamily: "Ranchers", color: Colors.black),
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: RichText(
+            text: TextSpan(
+              text: "US News App",
+              style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  fontSize: 18,
+                  color: Colors.grey),
+            ),
           ),
         ),
-      ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(12),
-              child: RichText(
-                text: TextSpan(
-                  text: "Discover!",
+        body: Body());
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(12),
+            child: RichText(
+              text: TextSpan(children: [
+                TextSpan(
+                  text: "Discover!\n",
                   style: TextStyle(
                     color: Colors.black,
-                    fontFamily: "Ranchers",
-                    letterSpacing: DimsConfig.configWidth(12),
+                    letterSpacing: DimsConfig.configWidth(6),
                     fontSize: 26,
                     fontWeight: FontWeight.w900,
                   ),
                 ),
-              ),
+                TextSpan(
+                  text: "Find out what is trending in these categories!",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: DimsConfig.configWidth(3),
+                    fontSize: 16,
+                  ),
+                ),
+              ]),
             ),
-            Categories()
-          ],
-        ),
-        width: double.infinity,
-        height: double.infinity,
+          ),
+          Categories(),
+          RandomlySelectedNews()
+        ],
       ),
+      width: double.infinity,
+      height: double.infinity,
     );
+  }
+}
+
+class RandomlySelectedNews extends StatefulWidget {
+  @override
+  _RandomlySelectedNewsState createState() => _RandomlySelectedNewsState();
+}
+
+class _RandomlySelectedNewsState extends State<RandomlySelectedNews> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }

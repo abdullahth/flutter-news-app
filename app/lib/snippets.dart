13794b0562043f18286dab06f6e73dbc8db8ea91
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-10-11 23:30:30 +0200

    Commit 1

--- app/lib/snippets.dart
+++ app/lib/snippets.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 var kCartegories = [
-  ["Football", "assets/images/football-icon.png", Colors.red],
-  ["Cinema", "assets/images/cinema-icon.png", Colors.yellow[900]],
-  ["Bussiness", "assets/images/bussiness-icon.png", Colors.blue]
+  ["Sports", "assets/images/football-icon.png", Colors.red[900]],
+  ["Entertainment", "assets/images/cinema-icon.png", Colors.yellow[900]],
+  ["Bussiness", "assets/images/bussiness-icon.png", Colors.blue[800]],
+  ["Science", "assets/images/science-icon.png", Colors.teal[900]]
 ];
+
+var kCategoriesName = ["sports", "entertainment", "bussiness", "science"];
+
+String apiLink(String category, {String country}) =>
+    "http://newsapi.org/v2/top-headlines?country=${country == null ? "us" : country}&category=${category == null ? "bussiness" : category}&apiKey=fff147fd795b46078be6cb160d355906";
